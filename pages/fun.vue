2f0e8290105fe54ed90d15147762be9ee11bbe18
adefilippi <anthony.defilippi@gmail.com>
2021-10-27 10:05:00 +0200

    first commit

---
+++ pages/fun.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "fun"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/loginWithToken.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "loginWithToken"
+}
+</script>
+
+<style scoped>
+
+</style>
